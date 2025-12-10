a785ed8ad80a73e44f7187994822ea7b36b4f2a9
Joshua <joshgonzalez027@gmail.com>
2025-12-10 14:52:41 -0400

    Adding main.h file with readme for new directory

---
+++ README.md
@@ -0,0 +1,1 @@
+Directory for the project C - Sorting algorithms & Big O. Functions and tasks
---
+++ sort.h
@@ -0,0 +1,37 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/**
+ * struct listint_s - Doubly linked list node structure
+ *
+ * @n: Integer value stored in the node
+ * @prev: Pointer to the previous node in the list
+ * @next: Pointer to the next node in the list
+ *
+ * Description: Node structure used for sorting algorithms that operate
+ * on doubly linked lists.
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+/* Printing Functions */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+/* Sorting Algorithms */
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+
+#endif
